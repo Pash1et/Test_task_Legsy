c84ddb5a1c3939236eca043b91a6e53a379cc1bc
Dmitry <d.gorelov22@gmail.com>
2023-04-13 16:02:21 +0300

    edit script.sh

--- infra/script.sh
+++ infra/script.sh
@@ -2,5 +2,4 @@
 
 alembic upgrade head
 
-#python3 main.py
-gunicorn main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
+gunicorn main:app --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
